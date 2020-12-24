e48a24fc0a098fc08ad30f36bd7f57f153cd5436
YFLooi <yihfoo@gmail.com>
2020-12-25 03:32:41 +0800

    working ts implementation

--- bjak_mongo-react-express-Pug-TS/client/src/App.js
+++ bjak_mongo-react-express-Pug-TS/client/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import OutputBox from './outputBox.tsx';
-import ReducerExample from './reducerExample.tsx';
 import { useState } from 'react';
 import { Button, InputGroup, FormControl } from 'react-bootstrap'; 
 
@@ -80,7 +79,7 @@ function App() {
   const handleNameSubmit = (method) => {
     if(method === "GET"){
       //Catch to stop on invalid input
-      if (name == "" || name == undefined){
+      if (name === "" || name === undefined){
         alert("No name submitted")
         return null;
       } 
@@ -96,7 +95,7 @@ function App() {
     }
     if(method === "POST"){
       //Catch to stop on invalid input
-      if (name == "" || name == undefined){
+      if (name === "" || name === undefined){
         alert("No name submitted")
         return null;
       } 
@@ -112,7 +111,7 @@ function App() {
     }
     if(method === "nameLookup"){
       //Catch to stop on invalid input
-      if (lookupName == "" || lookupName == undefined){
+      if (lookupName === "" || lookupName === undefined){
         alert("No name submitted")
         return null;
       } 
@@ -140,7 +139,7 @@ function App() {
     }
     if(method === "updateYear"){
       //Catch to stop on invalid input
-      if (updateYear[`year`] == "" || updateYear[`_id`] == "" || updateYear == undefined){
+      if (updateYear[`year`] === "" || updateYear[`_id`] === "" || updateYear === undefined){
         alert("Invalid year submitted")
         return null;
       } 
@@ -156,7 +155,7 @@ function App() {
     }
     if(method === "deleteCompany"){
       //Catch to stop on invalid input
-      if (delete_id == "" || delete_id == undefined){
+      if (delete_id === "" || delete_id === undefined){
         alert("Invalid _id submitted")
         return null;
       } 
@@ -181,75 +180,77 @@ function App() {
       <header className="App-header">
         Work on data from MongoDB with Mongoose
       </header>
-      <div className="body">
-        <div id="controlButtons">
-          <Button onClick={() =>{clearMessage();}}>Clear messages</Button>
-          <Button onClick={() =>{findTwenty();}}>findTwenty</Button>
-          <Button onClick={() =>{findTwentyWithSort("ASCENDING");}}>findTwentyWithSort - ASC</Button>
-          <Button onClick={() =>{findTwentyWithSort("DESCENDING");}}>findTwentyWithSort - DESC</Button>
-          
-        </div>
-        <div id="nameInput">
-          <div>Submit company name</div>
-          <div id="nameInput_input">
-            <InputGroup>
-              <InputGroup.Prepend>
-                <InputGroup.Text id="basic-addon1">Company name</InputGroup.Text>
-              </InputGroup.Prepend>
-              <FormControl
-                aria-label="companyName"
-                id="nameInputBox" type="text" placeholder="Type name..." value={name} 
+      <div id="main">
+        <div id="main_panel">
+          <div id="controlButtons">
+            <Button onClick={() =>{clearMessage();}}>Clear messages</Button>
+            <Button onClick={() =>{findTwenty();}}>findTwenty</Button>
+            <Button onClick={() =>{findTwentyWithSort("ASCENDING");}}>findTwentyWithSort - ASC</Button>
+            <Button onClick={() =>{findTwentyWithSort("DESCENDING");}}>findTwentyWithSort - DESC</Button>
+            
+          </div>
+          <div id="nameInput">
+            <div>Submit company name</div>
+            <div id="nameInput_input">
+              <InputGroup>
+                <InputGroup.Prepend>
+                  <InputGroup.Text id="basic-addon1">Company name</InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl
+                  aria-label="companyName"
+                  id="nameInputBox" type="text" placeholder="Type name..." value={name} 
+                  onChange={(event)=>{handleInputChange(event)}} 
+                />
+              </InputGroup>
+            </div>
+            <div id="nameInput_buttons">
+              <Button onClick={() =>{handleNameSubmit("GET");}}>Submit company name with GET</Button>
+              <Button onClick={() =>{handleNameSubmit("POST");}}>Submit company name with POST</Button>
+            </div>
+          </div>
+          <div id="nameLookup">
+            <div>Find first of company name</div>
+            <div>
+              <input 
+                id="nameLookupBox" type="text" placeholder="Type name..." 
+                value={lookupName} onChange={(event)=>{handleInputChange(event)}} 
+              />
+            </div>
+            <div>
+              <Button onClick={() =>{handleNameSubmit("nameLookup");}}>Submit</Button>
+            </div>
+          </div>
+          <div id="updateYear">
+            <div>Update year by company _id</div>
+            <div>
+              <input 
+                id="updateYearBox" type="text" placeholder="Type year..." 
+                value={updateYear["year"]} 
                 onChange={(event)=>{handleInputChange(event)}} 
               />
-            </InputGroup>
+              <input 
+                id="update_idBox" type="text" placeholder="Type _id..." 
+                value={updateYear["_id"]} 
+                onChange={(event)=>{handleInputChange(event)}} 
+              />
+            </div>
+            <div>
+              <Button onClick={() =>{handleNameSubmit("updateYear");}}>Submit</Button>
+            </div>
           </div>
-          <div id="nameInput_buttons">
-            <Button onClick={() =>{handleNameSubmit("GET");}}>Submit company name with GET</Button>
-            <Button onClick={() =>{handleNameSubmit("POST");}}>Submit company name with POST</Button>
-          </div>
-        </div>
-        <div id="nameLookup">
-          <div>Find first of company name</div>
-          <div>
-            <input 
-              id="nameLookupBox" type="text" placeholder="Type name..." 
-              value={lookupName} onChange={(event)=>{handleInputChange(event)}} 
-            />
-          </div>
-          <div>
-            <Button onClick={() =>{handleNameSubmit("nameLookup");}}>Submit</Button>
-          </div>
-        </div>
-        <div id="updateYear">
-          <div>Update year by company _id</div>
-          <div>
-            <input 
-              id="updateYearBox" type="text" placeholder="Type year..." 
-              value={updateYear["year"]} 
-              onChange={(event)=>{handleInputChange(event)}} 
-            />
-            <input 
-              id="update_idBox" type="text" placeholder="Type _id..." 
-              value={updateYear["_id"]} 
-              onChange={(event)=>{handleInputChange(event)}} 
-            />
-          </div>
-          <div>
-            <Button onClick={() =>{handleNameSubmit("updateYear");}}>Submit</Button>
-          </div>
-        </div>
-        <div id="deleteCompany">
-          <div>Delete entry by company _id</div>
-          <div>
-            <input id="delete_idBox" type="text" placeholder="Type _id..." value={delete_id} onChange={(event)=>{handleInputChange(event)}} />
-          </div>
-          <div>
-            <Button onClick={() =>{handleNameSubmit("deleteCompany");}}>Submit</Button>
+          <div id="deleteCompany">
+            <div>Delete entry by company _id</div>
+            <div>
+              <input id="delete_idBox" type="text" placeholder="Type _id..." value={delete_id} onChange={(event)=>{handleInputChange(event)}} />
+            </div>
+            <div>
+              <Button onClick={() =>{handleNameSubmit("deleteCompany");}}>Submit</Button>
+            </div>
           </div>
         </div>
         <div id="outputBox">{output}</div>
-        <OutputBox output={output}></OutputBox>
       </div>
+      <OutputBox output={output}></OutputBox> {/***/}
     </div>
   );
 }
--- bjak_mongo-react-express-Pug-TS/client/src/outputBox.tsx
+++ bjak_mongo-react-express-Pug-TS/client/src/outputBox.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 import { useState, useRef, FunctionComponent } from 'react';
-import { Button, InputGroup, FormControl } from 'react-bootstrap'; 
+import { Button } from 'react-bootstrap'; 
 
 //React TS files saved as .tsx, vanilla TS files saved as .ts
 
-//This type declaration interface applies to props being passed in
+//This type declaration interface applies to the "props" property being passed in
 //Type declarations examples for each data type
+//interface Props{} === type Props{} 
 //Optional fields marked with "?". These don't need to be included in props
 interface Props{
   output: (FunctionComponent | string)[];
@@ -13,9 +14,9 @@ interface Props{
   trueFalse?: boolean;
   counter?: number;
   customType?: CustomType;
-  dataArray: number[];
-  dataObject: object;
-  handleFunction: (p1: string, p2: number[]) => void
+  dataArray?: number[];
+  dataObject?: object;
+  handleFunction?: (p1: string, p2: number[]) => void
 }
 interface CustomType{
   firstName: string;
@@ -24,8 +25,9 @@ interface CustomType{
 
 
 //Ref: https://www.youtube.com/watch?v=Z5iWr6Srsj8&ab_channel=BenAwad
+//Ref: https://www.youtube.com/watch?v=F2JCjVSZlG0&ab_channel=freeCodeCamp.org 
 //If you'd like to include all Props{}, just place {} inside ()
-const OutputBox: React.FC<Props> = (props) => {
+const OutputBox: React.FC<Props> = ({ output }) => {
   //Declaring type using arrow syntax
   const [count, setCount] = useState<number | null>(5);
   //Declaring interface within arrow syntax
@@ -35,25 +37,34 @@ const OutputBox: React.FC<Props> = (props) => {
   const containerRef = useRef<HTMLDivElement>(null)
   const inputRef = useRef<HTMLInputElement>(null);
 
-  setCount(null);
-  setString({text: "running"})
+  //setCount(null);
+  //setString({text: "running"})
 
   //The type of each event here is obtained by hovering over onChange and onClick
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>):void => {
     console.log(`Attempted input into ${event.target.id}`)
   }
-  const handleInputSubmit = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>):void => {
+  const handleInputSubmit = ():void => {
     console.log(`"Submit" button clicked!`)
   }
 
   return (
-    <>
+    <div>
+      <h2>Child output from outputBox.tsx</h2>
       <div ref={containerRef}>
-        <input id= "dummyInput" ref={inputRef} onChange={handleInputChange} value={string["text"]}/>
-        <button onClick={handleInputSubmit}>Submit</button>
+        <input id= "dummyInput" ref={inputRef} onChange={(event) => {handleInputChange(event);}} value={string["text"]}/>
+        <Button onClick={(event) => {handleInputSubmit();}}>Submit</Button>
       </div>
-      <div id="outputBox">{props.output}</div>
-    </>
+      
+      {/**
+       * Notice the different way to pass props here in TS? 
+       * With vanilla React, its props.propName
+       * With TS, you first declare the prop's Type then bind it to the functional component (FC)
+       * The prop itself must be declared in an object inside the the FC's properties. 
+       * Only then can the prop be called in the FC
+       * */}
+      <div id="outputBox">{output}</div>
+    </div>
   );
 }
 
